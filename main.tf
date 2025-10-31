974f03b461795038b1ebd4d7fb7deec76a019016
Tagir Abdulkhaev <abtagir@gmail.com>
2025-10-31 16:10:22 +0100

    changed tomcat image again - now that worked!

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_task_definition" "ecs_task_def" {
 },
   {
     name      = "second"
-    image     = "tomcat:11.0.13-jdk17-temurin-jammy"
+    image     = "662793765491.dkr.ecr.eu-central-1.amazonaws.com/ecs_image:latest"
     essential = true
     portMappings = [{ containerPort = 8080 }]
     logConfiguration = {
