48144a77bbdebe20afdea08967e4b3726a9f9cd5
Tagir Abdulkhaev <abtagir@gmail.com>
2025-10-31 14:39:39 +0100

    added alb listener for port 8080

--- main.tf
+++ main.tf
@@ -192,6 +192,17 @@ resource "aws_lb_listener" "alb_listener" {
   }
 }
 
+resource "aws_lb_listener" "alb_listener_8080" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group2.arn
+  }
+}
+
 resource "aws_lb_target_group" "alb_target_group1" {
   name        = "alb-target-group-first"
   port        = 80
